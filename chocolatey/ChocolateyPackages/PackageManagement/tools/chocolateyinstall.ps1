cf7022e618303c43afe99df0d0c7ec1ab5f30c95
Ryan Conrad <camalot@gmail.com>
2015-10-25 16:58:12 -0500

    changed the packagemagement to use a simpler method of a zip file to install the package.

--- chocolatey/ChocolateyPackages/PackageManagement/tools/chocolateyinstall.ps1
+++ chocolatey/ChocolateyPackages/PackageManagement/tools/chocolateyinstall.ps1
@@ -1,31 +1,109 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$packageName = 'PackageManagement' # arbitrary name for the package, used in messages
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x86.msi' # download url
-$url64 = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x64.msi' # 64bit URL here or remove - if installer is both, use $url
-
-
-New-Item -Path "$env:TEMP\chocolatey\$packageName" -Force | Out-Null;
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'msi' #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url64
-
-  #MSI
-  silentArgs    = "/qn /norestart /l*v '$env:TEMP\chocolatey\$packageName\install.log'" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  # optional
-  registryUninstallerKey = '{57E5A8BB-41EB-4F09-B332-B535C5954A28}' #ensure this is the value in the registry
-  #checksum      = '{{Checksum}}'
-  #checksumType  = 'md5' #default is md5, can also be sha1
-  #checksum64    = '{{Checksumx64}}'
-  #checksumType64= 'md5' #default is checksumType
+﻿function Invoke-DownloadFile {
+	Param (
+		[string]$Url,
+		[string]$File
+	);
+	process {
+		"Downloading $Url to $File" | Write-Host;
+		$downloader = new-object System.Net.WebClient;
+		$downloader.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;
+		$downloader.DownloadFile($Url, $File);
+	}
 }
 
-Install-ChocolateyPackage @packageArgs
+function Expand-7ZipArchive {
+	Param (
+		[Parameter(Mandatory=$true, Position=0)]
+		[string] $Path,
+		[Parameter(Mandatory=$true, Position=1)]
+		[string] $DestinationPath
+	);
 
-## Main helper functions - these have error handling tucked into them already
-## see https://github.com/chocolatey/choco/wiki/HelpersReference
+	$scriptRootPath = Get-ScriptRoot;
+	$toolsDir = (Join-Path -Path $scriptRootPath -ChildPath "tools");
+
+	if(!(Test-Path -Path $toolsDir)) {
+		New-Item -Path $toolsDir -ItemType Directory | Out-Null;
+	}
+	$7zaExe = (Join-Path -Path $toolsDir -ChildPath "7za.exe");
+	if(!(Test-Path -Path $7zaExe)) {
+		# download 7zip
+		Write-Host "Download 7Zip commandline tool";
+		Invoke-DownloadFile -Url 'https://raw.githubusercontent.com/camalot/psievm/master/psievm/.tools/7za.exe' -File "$7zaExe";
+	}
+	Start-Process "$7zaExe" -ArgumentList "x -o`"$DestinationPath`" -y `"$Path`"" -Wait -NoNewWindow | Write-Host;
+}
+
+function Install-PackageManagement {
+	param (
+		[Parameter(Mandatory=$true)]
+		[string] $ModulesPath
+	);
+	begin {
+		$url = "http://de.bit13.com/psievm/PackageManagement.zip";
+
+		$tempDir = (Join-Path $env:TEMP "pm");
+		if(!(Test-Path -Path $tempDir)) {
+			New-Item -Path $tempDir -ItemType Directory | Out-Null;
+		}
+		$file = (Join-Path $tempDir "PackageManagement.zip");
+
+		if(!(Test-Path -Path $ModulesPath)) {
+			New-Item -Path $ModulesPath -ItemType Directory | Out-Null;
+		}
+	}
+	process {
+		Invoke-DownloadFile -url $url -file $file;
+		Expand-7ZipArchive -Path $file -DestinationPath $ModulesPath;
+		Get-ChildItem -Path "$ModulesPath" -File -Recurse | Unblock-File | Out-Null;
+	}
+}
+
+function Get-PSModulePath {
+
+	#$psmp = (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsPowerShell\Modules\");
+
+	#if(Test-Path -Path $psmp) {
+	#	return $psmp;
+	#}
+
+	$docsPath = (Get-EnvironmentFolderPath -Name MyDocuments);
+	if(-not $docsPath) {
+		# if MyDocuments doesn't give anything, use the user profile
+		return (Join-Path -Path $env:USERPROFILE -ChildPath "Documents\WindowsPowerShell\Modules\");
+	} else {
+		return (Join-Path -Path $docsPath -ChildPath "WindowsPowerShell\Modules\");
+	}
+}
+
+function Get-EnvironmentFolderPath {
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string] $Name
+	);
+
+	return [Environment]::GetFolderPath($Name);
+}
+
+function Get-ScriptRoot {
+	if(-not $PSScriptRoot) {
+		if(-not $PSCommandPath) {
+			return (Split-Path -Path $MyInvocation.MyCommand.Path -Parent);
+		} else {
+			return (Split-Path -Path $PSCommandPath -Parent);
+		}
+	} else {
+		return (Resolve-Path -Path $PSScriptRoot);
+	}
+}
+
+function Invoke-Setup {
+		$ModulesRoot = Get-PSModulePath;
+	Install-PackageManagement -ModulesPath $ModulesRoot;
+}
+
+
+
+if( ($DoSetup -eq $null) -or ($DoSetup -eq $true) ) {
+	Invoke-Setup;
+}
